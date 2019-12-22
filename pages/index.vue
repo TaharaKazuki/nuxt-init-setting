1d35abe709b08b6abf2d4aedc63145fff5edb1f0
田原 一樹 <ssp0727.lnc@gmail.com>
2019-12-22 22:59:23 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -27,11 +27,6 @@ import Logo from '~/components/Logo.vue'
 @Component({
   components: {
     Logo
-  },
-  methods: {
-    sample() {
-      console.info('通過')
-    }
   }
 })
 export default class IndexPage extends Vue {}
